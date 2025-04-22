ed9fe0b7f3d4cf1e0b31b6345faf7cfadae172d7
echarqykhadouj <echarqykhadouj@gmail.com>
2025-04-22 13:25:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Home from "./homePage/homePage";
+
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
---
+++ src/homePage/homePage.jsx
@@ -0,0 +1,13 @@
+import Footer from "../comparntt/Footer";
+import Header from "../comparntt/Header";
+import Main from "../comparntt/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Main />
+    </>
+  );
+}
